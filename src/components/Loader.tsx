f43c07c504477686eff349b09a26e6507db8bb0e
Paulo Wells <paulowells@gmail.com>
2025-02-15 22:38:30 -0300

    fixed issue with loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,8 +2,10 @@ import { IconLoader2 } from "@tabler/icons-react";
 
 export const Loader = () => {
     return (
-        <div className="loading">
-            <IconLoader2 size={30} />
+        <div className="loader">
+            <div className="loader__loading">
+                <IconLoader2 />
+            </div>
         </div>
     );
 };
